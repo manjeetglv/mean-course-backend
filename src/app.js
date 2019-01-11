f18da6ae5e4d4d8d7fcf18d0d5ac344726c87885
Manjeet Lama <manjeetlama@Manjeets-MBP.hitronhub.home>
2019-01-10 19:33:58 -0800

    body-parser and mongoose added

--- src/app.js
+++ src/app.js
@@ -1,16 +1,59 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('First middleware');
-    next();
-});
+// Body parser;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res) => {
-    res.send('Hello from express!');
+function allowCrossDomain() {
+    return function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    };
+}
+
+app.use(allowCrossDomain());
+
+app.post("/api/posts", (req, res, next) => {
+    console.log(req.body);
+    res.status(201).send();
+})
+app.get('/api/posts',(req, res) => {
+    const posts = [
+        {
+            id: "id1",
+            title: "First server-side post",
+            content: "This is comming from server"
+        },
+        {
+            id: "id2",
+            title: "Second server-side post",
+            content: "This is comming from server"
+        },
+        {
+            id: "id3",
+            title: "Third server-side post",
+            content: "This is comming from server"
+        },
+        {
+            id: "id4",
+            title: "Fourth server-side post",
+            content: "This is comming from server"
+        },
+        {
+            id: "id5",
+            title: "Fifth server-side post",
+            content: "This is comming from server"
+        }
+    ];
+
+    res.status(200).json(posts);
 });
 
 // Exporting the app, and then import in server.js
 // After that I will make this app the listner of the apps
 module.exports = app;
+
---
+++ src/model/post.model.js
@@ -0,0 +1,11 @@
+const mongoose  = require('mongoose');
+
+// Creating schema with mongoose.
+// This part is just schema definiton 
+const postSchema = mongoose.Schema({
+    title: {type: String, required: true},
+    content: {type: String, required: true}
+});
+
+// This model function create the model with the schema defined above.
+module.exports = mongoose.model('Post', postSchema); 
